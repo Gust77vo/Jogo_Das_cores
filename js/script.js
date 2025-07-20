19192a442d1bce29144ece3b4e6835618ad1d779
Gustvo77 <109292502+Gust77vo@users.noreply.github.com>
2025-07-20 14:16:40 -0300

    beta3

--- js/script.js
+++ js/script.js
@@ -8,27 +8,24 @@ const div7 = document.getElementById('div7');
 const div8 = document.getElementById('div8');
 const div9 = document.getElementById('div9');
 
-// Variável para controlar qual cor a div está no momento
-let estaAzul = true; 
-
-// Função que muda a cor da div
-function alternarCor() {
-    if (estaAzul) {
-        div1.style.backgroundColor = 'salmon'; // Muda para salmão
-        div2.style.backgroundColor = 'red';
-        div3.style.backgroundColor = 'blue';
-        div4.style.backgroundColor = 'blueviolet';
-        div5.style.backgroundColor = 'chartreuse';
-        div6.style.backgroundColor = 'yellow';
-        div7.style.backgroundColor = 'yellowgreen';
-        div8.style.backgroundColor = 'green';
-        div9.style.backgroundColor = 'darkcyan';
-    } else {
-        div1.style.backgroundColor = 'lightblue'; // Volta para azul claro
-    }
-    estaAzul = !estaAzul; // Inverte o estado da cor
+function ramdom(){
+    let colors = ['red','blue','black','yellow','green','darkcyan']
     
+    
+    return colors[ram];
 }
+function alternarCor() {
+    div1.style.backgroundColor = ramdom(); 
+    div2.style.backgroundColor = ramdom();
+    div3.style.backgroundColor = ramdom();
+    div4.style.backgroundColor = ramdom();
+    div5.style.backgroundColor = ramdom();
+    div6.style.backgroundColor = ramdom();
+    div7.style.backgroundColor = ramdom();
+    div8.style.backgroundColor = ramdom();
+    div9.style.backgroundColor = ramdom();
+}
+
 div1.onclick = function(){
     return alternarCor();
 }
