e92ab575f15d6d0b25e5c174b104126c3fe71888
Gustvo77 <109292502+Gust77vo@users.noreply.github.com>
2025-07-21 13:36:14 -0300

    beta5Fail

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@ const div6 = document.getElementById('div6');
 const div7 = document.getElementById('div7');
 const div8 = document.getElementById('div8');
 const div9 = document.getElementById('div9');
+const pontuacao = document.getElementById('pontuacao');
 const corEscolha = document.getElementById('escolha');
 
-function ramdom(listaColors){
+
+function ram(listaColors){
     for(let i = listaColors.length -1;i>0;i--){
         const j = Math.floor(Math.random() * (i+1));
         [listaColors[i],listaColors[j]] = [listaColors[j],listaColors[i]];
@@ -19,7 +21,9 @@ function ramdom(listaColors){
 
 function alternarCor() {
     let colors = ['red','blue','black','yellow','green','darkcyan','red','blue','black','yellow','green','darkcyan'];
-    let cor = ramdom(colors)
+    let cor = ram(colors);
+    let r = Math.floor(Math.random() * 8);
+    
     div1.style.backgroundColor = cor[0]; 
     div2.style.backgroundColor = cor[1];
     div3.style.backgroundColor = cor[2];
@@ -29,11 +33,11 @@ function alternarCor() {
     div7.style.backgroundColor = cor[6];
     div8.style.backgroundColor = cor[7];
     div9.style.backgroundColor = cor[8];
-
+    corEscolha.style.backgroundColor = cor[8];
 }
 function escolha(){
     alternarCor();
-
+    
 }
 div1.onclick = function(){
     escolha();
